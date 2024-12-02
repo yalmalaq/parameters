ef0ed4a8771ddf57f0bab61bd3676d34fe2ef842
Yazeed Almalaq <124050604+yalmalaq@users.noreply.github.com>
2024-12-02 15:26:19 +0000

    Update without_queue_size.c

--- without_queue_size.c
+++ without_queue_size.c
@@ -173,7 +173,7 @@ void* thread_function(void* arg) {
     pthread_exit(NULL);
 }
 
-void populate_queue_with_input(queue_t *q) {
+void queue1_values(queue_t *q) {
     for (int i = 0; i < Arr_size; i++) {
         int input = rand() % 100;
         write_element_to_queue(q, input);
@@ -193,7 +193,7 @@ int main(int argc, char *argv[]) {
     init_queue(&queue3);
     init_queue(&queue4);
 
-    populate_queue_with_input(&queue1);
+    queue1_values(&queue1);
 
 
     for (int i = 0; i < 4; i++) {
