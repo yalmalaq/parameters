98305724f1239d23e10f6a849f95b0c7ed082082
Yazeed Almalaq <124050604+yalmalaq@users.noreply.github.com>
2024-12-12 22:25:47 +0000

    Update with_queue_size.c

--- with_queue_size.c
+++ with_queue_size.c
@@ -103,53 +103,48 @@ void heavy_computation(int n) {
     }
 }
 
-void perform_stage(int stage, int thread_id) {
+void stage1(int thread_id) {
     int input;
-    switch (stage) {
-        case FUNC1:
-            pthread_mutex_lock(&queue_mutex[0]);
-            input = get_element_from_queue(&queue1);
-            pthread_mutex_unlock(&queue_mutex[0]);
-            if (input == TERM_TOKEN) return;
+    pthread_mutex_lock(&queue_mutex[0]);
+    input = get_element_from_queue(&queue1);
+    pthread_mutex_unlock(&queue_mutex[0]);
+    if (input == TERM_TOKEN) return;
 
-            int fib_result = fibonacci_iterative(input % 40);
-            heavy_computation(fib_result);
-            pthread_mutex_lock(&queue_mutex[1]);
-            write_element_to_queue(&queue2, fib_result, 1);
-            pthread_mutex_unlock(&queue_mutex[1]);
-            printf("Thread %d for Stage 1, input= %d, result= %d\n", thread_id, input, fib_result);
-            break;
+    int fib_result = fibonacci_iterative(input % 40);
+    heavy_computation(fib_result);
+    pthread_mutex_lock(&queue_mutex[1]);
+    write_element_to_queue(&queue2, fib_result, 1);
+    pthread_mutex_unlock(&queue_mutex[1]);
+    printf("Thread %d for Stage 1, input= %d, result= %d\n", thread_id, input, fib_result);
+}
 
-        case FUNC2:
-            pthread_mutex_lock(&queue_mutex[1]);
-            input = get_element_from_queue(&queue2);
-            pthread_mutex_unlock(&queue_mutex[1]);
-            if (input == TERM_TOKEN) return;
+void stage2(int thread_id) {
+    int input;
+    pthread_mutex_lock(&queue_mutex[1]);
+    input = get_element_from_queue(&queue2);
+    pthread_mutex_unlock(&queue_mutex[1]);
+    if (input == TERM_TOKEN) return;
 
-            int fib_stage2 = fibonacci_iterative(input % 40);
-            heavy_computation(fib_stage2);
-            pthread_mutex_lock(&queue_mutex[2]);
-            write_element_to_queue(&queue3, fib_stage2, 2);
-            pthread_mutex_unlock(&queue_mutex[2]);
-            printf("Thread %d for Stage 2, input= %d, result= %d\n", thread_id, input, fib_stage2);
-            break;
+    int fib_stage2 = fibonacci_iterative(input % 40);
+    heavy_computation(fib_stage2);
+    pthread_mutex_lock(&queue_mutex[2]);
+    write_element_to_queue(&queue3, fib_stage2, 2);
+    pthread_mutex_unlock(&queue_mutex[2]);
+    printf("Thread %d for Stage 2, input= %d, result= %d\n", thread_id, input, fib_stage2);
+}
 
-        case FUNC3:
-            pthread_mutex_lock(&queue_mutex[2]);
-            input = get_element_from_queue(&queue3);
-            pthread_mutex_unlock(&queue_mutex[2]);
-            if (input == TERM_TOKEN) return;
+void stage3(int thread_id) {
+    int input;
+    pthread_mutex_lock(&queue_mutex[2]);
+    input = get_element_from_queue(&queue3);
+    pthread_mutex_unlock(&queue_mutex[2]);
+    if (input == TERM_TOKEN) return;
 
-            unsigned long long fact_stage3 = factorial_iterative(input);
-            pthread_mutex_lock(&queue_mutex[3]);
-            write_element_to_queue(&queue4, fact_stage3, 3);
-            pthread_mutex_unlock(&queue_mutex[3]);
-            printf("Thread %d for Stage 3, input= %d, result= %llu\n", thread_id, input, fact_stage3);
-            break;
-
-        default:
-            printf("wrong stage %d\n", stage);
-    }
+    unsigned long long fact_stage3 = factorial_iterative(input);
+    pthread_mutex_lock(&queue_mutex[3]);
+    write_element_to_queue(&queue4, fact_stage3, 3);
+    pthread_mutex_unlock(&queue_mutex[3]);
+    printf("Thread %d for Stage 3, input= %d, result= %llu\n", thread_id, input, fact_stage3);
 }
 
 void* thread_function(void* arg) {
@@ -162,15 +157,25 @@ void* thread_function(void* arg) {
             pthread_mutex_lock(&queue_mutex[j]);
             input_queue_empty = empty_queue(j == 0 ? &queue1 : j == 1 ? &queue2 : &queue3);
             pthread_mutex_unlock(&queue_mutex[j]);
-            
+
             if (input_queue_empty) break;
 
-            perform_stage(config->stages[j], config->thread_id);
+            switch (config->stages[j]) {
+                case FUNC1:
+                    stage1(config->thread_id);
+                    break;
+                case FUNC2:
+                    stage2(config->thread_id);
+                    break;
+                case FUNC3:
+                    stage3(config->thread_id);
+                    break;
+            }
         }
 
         if (input_queue_empty) break;
     }
-    
+
     pthread_exit(NULL);
 }
 
@@ -180,7 +185,6 @@ void queue1_values(queue_t *q) {
         write_element_to_queue(q, input, 0);
     }
 }
-
 
 int main(int argc, char *argv[]) {
     if (argc < 5) {
@@ -198,7 +202,7 @@ int main(int argc, char *argv[]) {
     init_queue(&queue2, queue2_size);
     init_queue(&queue3, queue3_size);
     init_queue(&queue4, queue4_size);
-    
+
     queue1_values(&queue1);
 
     for (int i = 0; i < 4; i++) {
@@ -209,22 +213,18 @@ int main(int argc, char *argv[]) {
     thread_config_t thread_configs[MAX_THREADS];
 
     if (group == 1) {
-        // (stage1) , (stage2), (stage3)
         thread_configs[0] = (thread_config_t){ 0, { FUNC1 }, 1 };
         thread_configs[1] = (thread_config_t){ 1, { FUNC2 }, 1 };
         thread_configs[2] = (thread_config_t){ 2, { FUNC3 }, 1 };
     } else if (group == 2) {
-        // (stage1 + stage 2), (stage 3)
         thread_configs[0] = (thread_config_t){ 0, { FUNC1, FUNC2 }, 2 };
         thread_configs[1] = (thread_config_t){ 1, { FUNC3 }, 1 };
         thread_configs[2] = (thread_config_t){ 2, { FUNC3 }, 1 };
     } else if (group == 3) {
-        // (stage 1), (stage2 + stage3)
         thread_configs[0] = (thread_config_t){ 0, { FUNC1 }, 1 };
         thread_configs[1] = (thread_config_t){ 1, { FUNC1 }, 1 };
         thread_configs[2] = (thread_config_t){ 2, { FUNC2, FUNC3 }, 2 };
     } else if (group == 4) {
-        // (stage1 + stage2 + stages3)
         thread_configs[0] = (thread_config_t){ 0, { FUNC1, FUNC2, FUNC3 }, 3 };
         thread_configs[1] = (thread_config_t){ 1, { FUNC1, FUNC2, FUNC3 }, 3 };
         thread_configs[2] = (thread_config_t){ 2, { FUNC1, FUNC2, FUNC3 }, 3 };
