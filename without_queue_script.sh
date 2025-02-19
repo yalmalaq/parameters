b32b283fb6566fccb24155b813cffe0466b389a8
Yazeed Almalaq <124050604+yalmalaq@users.noreply.github.com>
2025-02-19 17:09:46 +0000

    Update without_queue_script.sh

--- without_queue_script.sh
+++ without_queue_script.sh
@@ -46,8 +46,8 @@ done
 ############### g2 balananced ###############
 for run1 in {1..5}; do
     echo "Run1 #$run1"
-    OUTPUT_FILE5="without_queue_size/output_BALANCED_3th_g2_21_arraysize${ARR_SIZE}_only_run${run1}_1B.txt"
-    sudo python3 INA219_energy_freq.py ./without_queue_size_1B_g2 $ARR_SIZE >> "$OUTPUT_FILE5"
+    OUTPUT_FILE17="without_queue_size/output_BALANCED_3th_g2_12_arraysize${ARR_SIZE}_only_run${run1}_1B.txt"
+    sudo python3 INA219_energy_freq.py ./without_queue_size_1B_g2 $ARR_SIZE >> "$OUTPUT_FILE17"
     sleep 5
 done
 
@@ -55,18 +55,8 @@ done
 ##############################
 for run1 in {1..5}; do
     echo "Run1 #$run1"
-    OUTPUT_FILE6="without_queue_size/output_BALANCED_3th_g2_21_arraysize${ARR_SIZE}_only_run${run1}_500M.txt"
-    sudo python3 INA219_energy_freq.py ./without_queue_size_500M_g2 $ARR_SIZE >> "$OUTPUT_FILE6"
-    sleep 5
-done
-
-
-
-##############################
-for run1 in {1..5}; do
-    echo "Run1 #$run1"
-    OUTPUT_FILE7="without_queue_size/output_BALANCED_3th_g2_21_arraysize${ARR_SIZE}_only_run${run1}_100M.txt"
-    sudo python3 INA219_energy_freq.py ./without_queue_size_100M_g2 $ARR_SIZE >> "$OUTPUT_FILE7"
+    OUTPUT_FILE18="without_queue_size/output_BALANCED_3th_g2_12_arraysize${ARR_SIZE}_only_run${run1}_500M.txt"
+    sudo python3 INA219_energy_freq.py ./without_queue_size_500M_g2 $ARR_SIZE >> "$OUTPUT_FILE18"
     sleep 5
 done
 
@@ -75,8 +65,59 @@ done
 ##############################
 for run1 in {1..5}; do
     echo "Run1 #$run1"
-    OUTPUT_FILE8="without_queue_size/output_BALANCED_3th_g2_21_arraysize${ARR_SIZE}_only_run${run1}_10M.txt"
-    sudo python3 INA219_energy_freq.py ./without_queue_size_10M_g2 $ARR_SIZE >> "$OUTPUT_FILE8"
+    OUTPUT_FILE19="without_queue_size/output_BALANCED_3th_g2_12_arraysize${ARR_SIZE}_only_run${run1}_100M.txt"
+    sudo python3 INA219_energy_freq.py ./without_queue_size_100M_g2 $ARR_SIZE >> "$OUTPUT_FILE19"
+    sleep 5
+done
+
+
+
+##############################
+for run1 in {1..5}; do
+    echo "Run1 #$run1"
+    OUTPUT_FILE20="without_queue_size/output_BALANCED_3th_g2_12_arraysize${ARR_SIZE}_only_run${run1}_10M.txt"
+    sudo python3 INA219_energy_freq.py ./without_queue_size_10M_g2 $ARR_SIZE >> "$OUTPUT_FILE20"
+    sleep 5
+done
+
+##############################
+
+
+
+############### g3 balananced ###############
+for run1 in {1..5}; do
+    echo "Run1 #$run1"
+    OUTPUT_FILE5="without_queue_size/output_BALANCED_3th_g3_21_arraysize${ARR_SIZE}_only_run${run1}_1B.txt"
+    sudo python3 INA219_energy_freq.py ./without_queue_size_1B_g3 $ARR_SIZE >> "$OUTPUT_FILE5"
+    sleep 5
+done
+
+
+##############################
+for run1 in {1..5}; do
+    echo "Run1 #$run1"
+    OUTPUT_FILE6="without_queue_size/output_BALANCED_3th_g3_21_arraysize${ARR_SIZE}_only_run${run1}_500M.txt"
+    sudo python3 INA219_energy_freq.py ./without_queue_size_500M_g3 $ARR_SIZE >> "$OUTPUT_FILE6"
+    sleep 5
+done
+
+
+
+##############################
+for run1 in {1..5}; do
+    echo "Run1 #$run1"
+    OUTPUT_FILE7="without_queue_size/output_BALANCED_3th_g3_21_arraysize${ARR_SIZE}_only_run${run1}_100M.txt"
+    sudo python3 INA219_energy_freq.py ./without_queue_size_100M_g3 $ARR_SIZE >> "$OUTPUT_FILE7"
+    sleep 5
+done
+
+
+
+##############################
+for run1 in {1..5}; do
+    echo "Run1 #$run1"
+    OUTPUT_FILE8="without_queue_size/output_BALANCED_3th_g3_21_arraysize${ARR_SIZE}_only_run${run1}_10M.txt"
+    sudo python3 INA219_energy_freq.py ./without_queue_size_10M_g3 $ARR_SIZE >> "$OUTPUT_FILE8"
     sleep 5
 done
 
@@ -111,37 +152,37 @@ done
     echo " without_queue_size_500M_500M_1B 5 runs finished"
 ##############################
 
-############# unbalanced g2 #############
+############# unbalanced g3 #############
 for run1 in {1..5}; do
     echo "Run1 #$run1"
-    OUTPUT_FILE12="without_queue_size/output_UNBALANCED_3th_g2_21_arraysize${ARR_SIZE}_only_run${run1}_500M_1B.txt"
-    sudo python3 INA219_energy_freq.py ./without_queue_size_500M_1B_g2 $ARR_SIZE >> "$OUTPUT_FILE12"
+    OUTPUT_FILE12="without_queue_size/output_UNBALANCED_3th_g3_21_arraysize${ARR_SIZE}_only_run${run1}_500M_1B.txt"
+    sudo python3 INA219_energy_freq.py ./without_queue_size_500M_500M_1B_g3 $ARR_SIZE >> "$OUTPUT_FILE12"
     sleep 5
 done
     echo " without_queue_size_500M_1B_g2 5 runs finished"
 ##############################
 for run1 in {1..5}; do
     echo "Run1 #$run1"
-    OUTPUT_FILE13="without_queue_size/output_UNBALANCED_3th_g2_21_arraysize${ARR_SIZE}_only_run${run1}_1B_500M.txt"
-    sudo python3 INA219_energy_freq.py ./without_queue_size_1B_500M_g2 $ARR_SIZE >> "$OUTPUT_FILE13"
+    OUTPUT_FILE13="without_queue_size/output_UNBALANCED_3th_g3_21_arraysize${ARR_SIZE}_only_run${run1}_1B_500M.txt"
+    sudo python3 INA219_energy_freq.py ./without_queue_size_1B_1B_500M_g3 $ARR_SIZE >> "$OUTPUT_FILE13"
     sleep 5
 done
     echo " without_queue_size_1B_500M_g2 5 runs finished"
 ##############################
 
-############# unbalanced g3 #############
+############# unbalanced g2 #############
 for run1 in {1..5}; do
     echo "Run1 #$run1"
-    OUTPUT_FILE14="without_queue_size/output_UNBALANCED_3th_g3_12_arraysize${ARR_SIZE}_only_run${run1}_500M_1B.txt"
-    sudo python3 INA219_energy_freq.py ./without_queue_size_500M_1B_g3 $ARR_SIZE >> "$OUTPUT_FILE14"
+    OUTPUT_FILE14="without_queue_size/output_UNBALANCED_3th_g2_12_arraysize${ARR_SIZE}_only_run${run1}_500M_1B.txt"
+    sudo python3 INA219_energy_freq.py ./without_queue_size_500M_1B__1B_g2 $ARR_SIZE >> "$OUTPUT_FILE14"
     sleep 5
 done
     echo " without_queue_size_500M_1B_g3 5 runs finished"
 ##############################
 for run1 in {1..5}; do
     echo "Run1 #$run1"
-    OUTPUT_FILE15="without_queue_size/output_UNBALANCED_3th_g3_12_arraysize${ARR_SIZE}_only_run${run1}_1B_500M.txt"
-    sudo python3 INA219_energy_freq.py ./without_queue_size_1B_500M_g3 $ARR_SIZE >> "$OUTPUT_FILE15"
+    OUTPUT_FILE15="without_queue_size/output_UNBALANCED_3th_g2_12_arraysize${ARR_SIZE}_only_run${run1}_1B_500M.txt"
+    sudo python3 INA219_energy_freq.py ./without_queue_size_1B_500M_500M_g2 $ARR_SIZE >> "$OUTPUT_FILE15"
     sleep 5
 done
     echo " without_queue_size_1B_500M_g3 5 runs finished"
